950f225448ca24b50923a404efafdde232eba6b5
Paul-Albert Chmilevski <albert.chmilevski@gmail.com>
2014-06-01 20:36:29 +0300

    Initial page structure

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>OnCo</title>
+    <link href="css/style.css" rel="stylesheet" type="text/css"/>
+</head>
+<body>
+    <header>
+        <p>Bara cu userul logat si chestii.</p>
+    </header>
+    <nav>
+        <p>Meniu cu navigatie, ma gandesc ce punem aici.</p>
+    </nav>
+    <aside>
+        <p>Aici vor aparea grupurile de contacte</p>
+    </aside>
+    <section>
+        <p>Aici punem Contactele.</p>
+    </section>
+    <footer>
+        <p>Footer cu copyright.</p>
+    </footer>
+</body>
+</html>
