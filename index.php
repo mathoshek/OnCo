a1755e97ef63df2c646285672101ea6b7d16b37f
Paul-Albert Chmilevski <albert.chmilevski@gmail.com>
2014-06-07 12:40:40 +0300

    Login Page

--- index.php
+++ index.php
@@ -9,24 +9,24 @@
     <link href="css/style.css" rel="stylesheet" type="text/css"/>
 </head>
 <body>
-    <header>
-        <div id="logo">OnCo</div>
-        <div id="user_control">
-Test
-        </div>
-        <div style="clear:both"></div>
-    </header>
-    <nav>
-        <p>Meniu cu navigatie, ma gandesc ce punem aici.</p>
-    </nav>
-    <aside>
-        <p>Aici vor aparea grupurile de contacte</p>
-    </aside>
-    <section>
-        <p>Aici punem Contactele.</p>
-    </section>
-    <footer>
-        <p>Footer cu copyright.</p>
-    </footer>
+<header>
+    <div id="logo">OnCo</div>
+    <div id="user_control">
+        Test
+    </div>
+    <div style="clear:both"></div>
+</header>
+<nav>
+    <p>Meniu cu navigatie, ma gandesc ce punem aici.</p>
+</nav>
+<aside>
+    <p>Aici vor aparea grupurile de contacte</p>
+</aside>
+<section id="contentLoggedIn">
+    <p>Aici punem Contactele.</p>
+</section>
+<footer>
+    <p>Footer cu copyright.</p>
+</footer>
 </body>
 </html>
